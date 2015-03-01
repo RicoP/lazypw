17eab84577d85f68f7261d47252df69688b402cd
Rico P <mailtorico@gmail.com>
2015-03-01 23:39:45 +0100

    add base64 encoder

--- lazypw/main.cpp
+++ lazypw/main.cpp
@@ -1,3 +1,5 @@
+//sha512
+
 #include <string.h>
 
 #if defined(_MSC_VER) || defined(__WATCOMC__)
@@ -422,6 +424,86 @@ void sha512(const unsigned char *input, size_t ilen,
 	sha512_free(&ctx);
 }
 
+//base64
+
+#include <inttypes.h>
+#include <string.h>
+
+int base64encode(const void* data_buf, size_t dataLength, char* result, size_t resultSize)
+{
+	const char base64chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+#";
+	const uint8_t *data = (const uint8_t *)data_buf;
+	size_t resultIndex = 0;
+	size_t x;
+	uint32_t n = 0;
+	int padCount = dataLength % 3;
+	uint8_t n0, n1, n2, n3;
+
+	/* increment over the length of the string, three characters at a time */
+	for (x = 0; x < dataLength; x += 3)
+	{
+		/* these three 8-bit (ASCII) characters become one 24-bit number */
+		n = ((uint32_t)data[x]) << 16; //parenthesis needed, compiler depending on flags can do the shifting before conversion to uint32_t, resulting to 0
+
+		if ((x + 1) < dataLength)
+			n += ((uint32_t)data[x + 1]) << 8;//parenthesis needed, compiler depending on flags can do the shifting before conversion to uint32_t, resulting to 0
+
+		if ((x + 2) < dataLength)
+			n += data[x + 2];
+
+		/* this 24-bit number gets separated into four 6-bit numbers */
+		n0 = (uint8_t)(n >> 18) & 63;
+		n1 = (uint8_t)(n >> 12) & 63;
+		n2 = (uint8_t)(n >> 6) & 63;
+		n3 = (uint8_t)n & 63;
+
+		/*
+		* if we have one byte available, then its encoding is spread
+		* out over two characters
+		*/
+		if (resultIndex >= resultSize) return 1;   /* indicate failure: buffer too small */
+		result[resultIndex++] = base64chars[n0];
+		if (resultIndex >= resultSize) return 1;   /* indicate failure: buffer too small */
+		result[resultIndex++] = base64chars[n1];
+
+		/*
+		* if we have only two bytes available, then their encoding is
+		* spread out over three chars
+		*/
+		if ((x + 1) < dataLength)
+		{
+			if (resultIndex >= resultSize) return 1;   /* indicate failure: buffer too small */
+			result[resultIndex++] = base64chars[n2];
+		}
+
+		/*
+		* if we have all three bytes available, then their encoding is spread
+		* out over four characters
+		*/
+		if ((x + 2) < dataLength)
+		{
+			if (resultIndex >= resultSize) return 1;   /* indicate failure: buffer too small */
+			result[resultIndex++] = base64chars[n3];
+		}
+	}
+
+	/*
+	* create and add padding that is required if we did not have a multiple of 3
+	* number of characters available
+	*/
+	if (padCount > 0)
+	{
+		for (; padCount < 3; padCount++)
+		{
+			if (resultIndex >= resultSize) return 1;   /* indicate failure: buffer too small */
+			result[resultIndex++] = '=';
+		}
+	}
+	if (resultIndex >= resultSize) return 1;   /* indicate failure: buffer too small */
+	result[resultIndex] = 0;
+	return 0;   /* indicate success */
+}
+
 //MAIN 
 
 #include <stdio.h>
@@ -430,6 +512,9 @@ int main(int argc, char** argv) {
 	unsigned char foo[] = "hello";
 	unsigned char output[64];
 
+	char base64out[256];
+	memset(base64out, 0, 256);
+
 	sha512(foo, 5, output, 0);
 
 	for (int i = 0; i < 64; ++i) {
@@ -437,6 +522,10 @@ int main(int argc, char** argv) {
 	}
 
 	printf("\n");
+
+	base64encode("hello world", 11, base64out, 256);
+
+	printf("%s\n", base64out);
 
 	return 0;
 }
