9bbbce85de36e07afdf2ba4e6bce67a68e295fd9
Rico P <mailtorico@gmail.com>
2015-03-02 00:37:33 +0100

    remove ifndef

--- lazypw/utils.h
+++ lazypw/utils.h
@@ -1,11 +1,7 @@
 #ifndef UTILS_H
 #define UTILS_H
 
-#ifdef _MSC_VER 
-#include <type_traits>
-#include <iosfwd>
 #define STATIC_ASSERT(x) static_assert(x, #x)
-#else
-#define STATIC_ASSERT(x) 
-#endif 
+#define SPRINTF sprintf_s
+
 #endif
