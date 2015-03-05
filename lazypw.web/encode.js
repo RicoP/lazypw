c89f0e68ca72799fccd1bf88acbb173c3c3ef1df
Rico Possienka <rico@blackpantsstudio.com>
2015-03-05 18:27:06 +0100

    testcode for angular js

---
+++ lazypw.web/encode.js
@@ -0,0 +1,9 @@
+﻿var _encode = Module.cwrap('encode', // name of C function
+'string', // return type
+['string']); // argument types
+
+
+function encode(text) {
+    var ret = _encode(text);
+    return ret;
+}
---
+++ lazypw.web/test.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    angular.module('lazyApp', []).controller('LazyController', ['$scope', Ctrl]);
+
+    function Ctrl($scope) {
+        $scope.accounts = [
+        { name: 'learn angular' },
+        { name: 'build an angular app' }];
+
+        $scope.addAccount = function () {
+            $scope.accounts.push({ name: $scope.accountText, encoded: encode($scope.accountText) });
+            $scope.accountText = '';
+        };
+    }
+})();
