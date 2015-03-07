cc7520097d00bd6c4694edfe7c46f7bdcce667e2
Rico P <mailtorico@gmail.com>
2015-03-07 14:27:08 +0100

    use test data

--- lazypw.web/logic.ts
+++ lazypw.web/logic.ts
@@ -1,16 +1,36 @@
 ﻿declare var angular: any;
 
+interface Account {
+    name: string;
+    encoded: string;
+}
+
+interface Scope {
+    accountText: string;
+    accounts: Account[];
+    addAccount: () => void;
+}
+
 (function () {
     angular.module('lazyApp', []).controller('LazyController', ['$scope', Ctrl]);
 
-    function Ctrl($scope) {
-        $scope.accounts = [
-        { name: 'learn angular' },
-        { name: 'build an angular app' }];
+    function Ctrl($scope: Scope) {
+        function isValid(name: string) {
+            return name.trim() != "";
+        }
+
+        var testAccounts = ["google", "reddit", "facebook"];
+
+        $scope.accounts = testAccounts.map(a => { return { name: a, encoded: "" }; });
+        $scope.accountText = "";
 
         $scope.addAccount = function () {
-            $scope.accounts.push({ name: $scope.accountText, encoded: encode($scope.accountText) });
+            var account = $scope.accountText.trim();
             $scope.accountText = '';
+
+            if (isValid(account)) {
+                $scope.accounts.push({ name: account, encoded: encode(account) });
+            }
         };
     }
 })();
